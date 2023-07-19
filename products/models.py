43df836eae0e35d36374e42e1b36ca5fac3b7a36
mohammad <mohammadhp858@gmail.com>
2023-07-19 12:27:43 +0330

    fix: modify category, product, comment feat: create brand model

--- products/models.py
+++ products/models.py
@@ -1,24 +1,36 @@
 from django.db import models
 from core.models import BaseModel
+from accounts.models import User
 
 class Category(BaseModel):
     name = models.CharField(max_length=150)
-    slug = models.CharField(max_length=150)
+    slug = models.SlugField()
     description = models.CharField(max_length=255)
-    parent = models.ForeignKey(to='Category', on_delete=models.SET_NULL)
+    parent = models.ForeignKey(to='Category', on_delete=models.SET_NULL, null=True)
+
+
+
 
 
 class Product(BaseModel):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+    slug = models.SlugField()
     description = models.CharField(max_length=255)
-    is_available = models.BooleanField(default=True)
+    inventory = models.PositiveSmallIntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='media')
     max_order = models.PositiveSmallIntegerField()
 
 
-# class Comment(BaseModel):
-#     user = ...
-#     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
-#     content = models.CharField(max_length=255)
+class Brand(BaseModel):
+    brand_name = models.CharField(max_length=250)
+    logo = models.ImageField(upload_to='media/')
+    products = models.ForeignKey(to=Product, on_delete=models.CASCADE, null=True)
+    
+
+
+class Comment(BaseModel):
+    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
