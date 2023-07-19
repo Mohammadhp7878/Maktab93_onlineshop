4af6515fb158d770c019a2344583371df015af7e
mohammad <mohammadhp858@gmail.com>
2023-07-20 00:41:59 +0330

    fix: change model issues

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-
+from .models import User
 
 class RegisterForm(forms.ModelForm):
-    phone_number = forms.EmailField()
+    phone_number = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['phone_number']
