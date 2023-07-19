358d97bfd5531b72c9285de667d756e28f89b808
mohammad <mohammadhp858@gmail.com>
2023-07-20 00:45:22 +0330

    feat: complete registration class

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import RegisterForm
-
+import random
+from utilize import send_otp
 class Register(View):
     reg_template = 'register.html'
-    form = RegisterForm
+    reg_form = RegisterForm
 
     def get(self, request):
-        return render(request, self.reg_template, self.form)
+        form = self.reg_form()
+        return render(request, self.reg_template, {'form':form})
+    
+    def post(self, request):
+        form = self.reg_form(request.POST)
 
+        if form.is_valid():
+            phone_number = form.cleaned_data['phone_number']
+            code = random.randint(10000, 99999)
+            send_otp(phone_number, code)
+            request.session['register_form'] = {'phone_number':phone_number}
+            return redirect('verify_code')
+
+class Verify(View):
+    def get(self, request):
+        ...
