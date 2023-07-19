e1104606b031e4f29aeab22933caa6a0aca26db9
mohammad <mohammadhp858@gmail.com>
2023-07-20 01:51:45 +0330

    feat: register Order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['status', 'deliver_time', 'total_price']
+    list_filter = ['status']
