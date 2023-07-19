b5e345716b16236e3dcbeb8d23a47624f72164a5
mohammad <mohammadhp858@gmail.com>
2023-07-19 22:53:55 +0330

    feat: create register view(get part)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import RegisterForm
 
-# Create your views here.
+class Register(View):
+    reg_template = 'register.html'
+    form = RegisterForm
+
+    def get(self, request):
+        return render(request, self.reg_template, self.form)
+
