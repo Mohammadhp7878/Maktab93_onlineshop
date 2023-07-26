ca068098703730fd3fc637067abfe17cfc2d4eb5
mohammad <mohammadhp858@gmail.com>
2023-07-26 14:33:16 +0330

    fix: remove junk api view

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products_api', views.Home.as_view(), name='home_products'),
     path('', views.home, name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -5,8 +5,3 @@ from rest_framework.views import APIView
 
 def home(request):
     return render(request, 'home.html') 
-
-class Home(APIView):
-    def get(self, request):
-        content = {"hello": "world", "title": "home"}
-        return Response(data=content)
