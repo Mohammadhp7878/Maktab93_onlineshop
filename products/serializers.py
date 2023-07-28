285a5f033721ad7d0cee9eb8bd885f616242d02c
mohammad <mohammadhp858@gmail.com>
2023-07-28 16:29:20 +0330

    add brand, product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 from . import models
 
 
-class Comment(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = models.Product
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
-        fields = ['name', 'description', 'parent']
+        fields = ["name", "description", "parent"]
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = models.Brand
+        field = '__all__'
