09de87a581b1c59fff4f5a3982da9b4f71660a03
mohammad <mohammadhp858@gmail.com>
2023-08-02 00:05:11 +0330

    fix: change views to view set

--- products/views.py
+++ products/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from django.views import View
-from rest_framework import generics
+from rest_framework import generics, viewsets, permissions
 from . import serializers
 from . import models
+from .permissions import IsCommentOwner
 
 
 class ProductsPage(View):
     def get(self, request):
         return render(request, 'products.html')
-class ProductList(generics.ListAPIView):
-    queryset = models.Product.objects.all()
-    serializer_class = serializers.ProductSerializer
-
-class SingleProduct(generics.RetrieveAPIView):
-    queryset = models.Product.objects.all()
-    serializer_class = serializers.ProductSerializer
+    
     
 class CategoryView(generics.ListAPIView):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
+    
+    
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+    
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, IsCommentOwner]
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer    
