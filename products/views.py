d056a0b91bc9e1049e3ae669f8e0be628e7337c2
mohammad <mohammadhp858@gmail.com>
2023-08-02 00:35:42 +0330

    fix: change the query and add get_context

--- products/views.py
+++ products/views.py
@@ -23,5 +23,11 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsCommentOwner]
-    queryset = models.Comment.objects.all()
-    serializer_class = serializers.CommentSerializer    
+    serializer_class = serializers.CommentSerializer  
+    
+    def get_queryset(self):
+        #overwrite our get_queryset to retrieve objects for specific products
+        return models.Comment.objects.filter(products=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
