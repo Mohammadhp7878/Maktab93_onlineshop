ddf7258d122d05516dddfecd69250f14eed8e9a6
mohammad <mohammadhp858@gmail.com>
2023-08-02 23:03:21 +0330

    feat: add gallery serializer, discount field

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from . import models
 
 
+class GallerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Gallery
+        fields = "__all__"
+
+
 class NestedCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
@@ -20,9 +26,9 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = ["id", "user", "content"]
-         
+
     def create(self, validated_data):
-        product_id = self.context['product_id']
+        product_id = self.context["product_id"]
         if product_id:
             product = models.Product.objects.get(pk=product_id)
             return models.Comment.objects.create(products=product, **validated_data)
@@ -31,11 +37,28 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True)
+    main_image = serializers.SerializerMethodField()
+    discount_to_price = serializers.FloatField(read_only=True)
 
     class Meta:
         model = models.Product
-        fields = ["id", "name", "description", "price", "images", "comments", "brand"]
+        fields = (
+            "id",
+            "name",
+            "categories",
+            "slug",
+            "description",
+            "inventory",
+            "price",
+            "main_image",
+            "max_order",
+            "brand",
+            "discount_to_price"
+        )
+
+    def get_main_image(self, product):
+        main_image = product.images
+        return GallerySerializer(main_image).data
 
 
 class BrandSerializer(serializers.ModelSerializer):
