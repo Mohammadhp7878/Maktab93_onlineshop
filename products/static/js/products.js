d8d9a95d3b26f8af50a47ff17a9b29055dfca58b
mohammad <mohammadhp858@gmail.com>
2023-08-02 23:03:45 +0330

    fix: show image and discount price

--- products/static/js/products.js
+++ products/static/js/products.js
@@ -16,15 +16,15 @@
                 <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
                     <div class="card product-item border-0 mb-4">
                         <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                            <img class="img-fluid w-100" src="${product.image}" alt="${product.name}">
+                            <img class="img-fluid w-100" src="${product.main_image.image_url}" alt="${product.main_image.alt}">
                         </div>
                         <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
                             <h6 class="text-truncate mb-3">${product.name}</h6>
-                            <div class="d-flex justify-content-center">
-                                <h6>${product.price}</h6><h6 class="text-muted ml-2"><del>${product.old_price}</del></h6>
-                            </div>
-                        </div>
-                        <div class="card-footer d-flex justify-content-between bg-light border">
+                            ${
+                                product.discount_to_price > 0
+                                    ? `<h6><del>${product.price}</del> ${product.discount_to_price}</h6>`
+                                    : `<h6>${product.price}</h6>`
+                            }
                             <a href="${product.detail_url}" class="btn btn-sm text-dark p-0">
                                 <i class="fas fa-eye text-primary mr-1"></i>View Detail
                             </a>
