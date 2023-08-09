afd36250d1bde1ff75ffbee3399a5a76b46fef5a
mohammad <mohammadhp858@gmail.com>
2023-08-09 15:34:56 +0330

    feat: view to show cart and cart item

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views import View
-
-
-
+from .models import Cart
+from .serializers import CartSerializer
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 
 class CartPage(View):
     def get(self, request):
         return render(request, 'cart.html')
 
 
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Cart.objects.prefetch_related('cartproducts__products').all()
+    serializer_class = CartSerializer 
+    
