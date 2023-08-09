13f0b8f42361adfa0eae3ba0c693b7a739c7e2d9
mohammad <mohammadhp858@gmail.com>
2023-08-09 15:31:50 +0330

    feat: calculate total price of cart

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,19 +6,26 @@ from . import models
     
 class CartProductSerializer(serializers.ModelSerializer):
         products = SimpleProductSerializer()
-        total_price = serializers.SerializerMethodField()
+        item_total_price = serializers.SerializerMethodField()
         
         class Meta:
             model = models.CartProduct
-            fields = ["id", "products", "quantity", "total_price"]
+            fields = ["id", "products", "quantity", "item_total_price"]
 
-        def get_total_price(self, cart_products):
-            return cart_products.quantity * cart_products.price
+        def get_item_total_price(self, cart_products:models.CartProduct):
+            if cart_products.products.discount:
+                return cart_products.quantity * cart_products.products.discount_to_price
+            return cart_products.quantity * cart_products.products.price
 
 
 class CartSerializer(serializers.ModelSerializer):
-    items = CartProductSerializer(many=True)
-    
+    cartproducts = CartProductSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
     class Meta:
         model = models.Cart
-        fields = ["id", "items"]
+        fields = ["id", "cartproducts", "total_price"]
+        
+    def get_total_price(self, cart:models.Cart):
+        return sum([item.quantity * item.products.price if
+                not item.products.discount else item.quantity * item.products.discount_to_price
+                for item in cart.cartproducts.all()])
