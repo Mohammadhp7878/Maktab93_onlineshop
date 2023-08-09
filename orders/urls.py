6cfb4b16e110f40d239ca19a8f732e781694d8f1
mohammad <mohammadhp858@gmail.com>
2023-08-09 16:48:28 +0330

    feat: add route for cart items

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from . import views
 from products.urls import router
+from rest_framework_nested import routers
 
 
 router.register('cart_api', views.CartViewSet, basename='cart')
+cart_router = routers.NestedDefaultRouter(router, 'cart_api', lookup='cart')
+cart_router.register('cart_products', views.CartProductsViewSet, basename='cart_product')
+
 
 urlpatterns = [
     path('cart/', views.CartPage.as_view(), name='cart_page'),
     path('', include(router.urls)),
+    path('', include(cart_router.urls)),
     
 ]
