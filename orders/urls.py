44db692ed298c9c0ce34bfc7a2e71fa3c8755846
mohammad <mohammadhp858@gmail.com>
2023-08-09 15:35:25 +0330

    feat: add router for cart

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from products.urls import router
+
+
+router.register('cart_api', views.CartViewSet, basename='cart')
 
 urlpatterns = [
-    path('cart/', views.CartPage.as_view(), name='cart_page')
+    path('cart/', views.CartPage.as_view(), name='cart_page'),
+    path('', include(router.urls)),
+    
 ]
