3aea9272e640b7d061b56e81795887c23469735e
mohammad <mohammadhp858@gmail.com>
2023-08-09 22:29:46 +0330

    style: implement some pep8 roles

--- orders/views.py
+++ orders/views.py
@@ -5,24 +5,27 @@ from .serializers import CartSerializer, CartProductSerializer, AddProductSerial
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 
+
 class CartPage(View):
     def get(self, request):
-        return render(request, 'cart.html')
+        return render(request, "cart.html")
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
-    queryset = Cart.objects.prefetch_related('cartproducts__products').all()
-    serializer_class = CartSerializer 
+    queryset = Cart.objects.prefetch_related("cartproducts__products").all()
+    serializer_class = CartSerializer
 
 
 class CartProductsViewSet(ModelViewSet):
     def get_queryset(self):
-        return CartProduct.objects.filter(carts=self.kwargs['cart_pk']).select_related('products')
-    
+        return CartProduct.objects.filter(carts=self.kwargs["cart_pk"]).select_related(
+            "products"
+        )
+
     def get_serializer_class(self):
-        if self.request.method == 'POST':
+        if self.request.method == "POST":
             return AddProductSerializer
         return CartProductSerializer
-    
+
     def get_serializer_context(self):
-        return {'cart_id':self.kwargs['cart_pk']}
+        return {"cart_id": self.kwargs["cart_pk"]}
