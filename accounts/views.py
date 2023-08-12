a6f747d4440c668125ec5c8e25f7e8b80e3dcc6a
mohammad <mohammadhp858@gmail.com>
2023-08-12 11:40:39 +0330

    fix: recreate login page using API

--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,50 @@
 from django.shortcuts import render, redirect
-from django.views import View
-from .forms import RegisterForm, OtpCodeForm
+from rest_framework.views import APIView
 import random
 from utilize import send_otp
+from .serializers import PhoneSerializer
 
 
-class Register(View):
-    reg_template = "login.html"
-    reg_form = RegisterForm
 
-    def get(self, request):
-        form = self.reg_form()
-        return render(request, self.reg_template, {"form": form})
-
+class LoginAPI(APIView):
     def post(self, request):
-        form = self.reg_form(request.POST)
-
-        if form.is_valid():
-            phone_number = form.cleaned_data["phone_number"]
+        serializer = PhoneSerializer(data=request.data)
+        if serializer.is_valid():
+            phone_number = serializer.validated_data[phone_number]
             code = random.randint(10000, 99999)
             send_otp(phone_number, code)
-            request.session["register_form"] = {"phone_number": phone_number}
-            return redirect("verify_code")
+        
+
+# class Register(View):
+#     reg_template = "login.html"
+#     reg_form = RegisterForm
+
+#     def get(self, request):
+#         form = self.reg_form()
+#         return render(request, self.reg_template, {"form": form})
+
+#     def post(self, request):
+#         form = self.reg_form(request.POST)
+
+#         if form.is_valid():
+#             phone_number = form.cleaned_data["phone_number"]
+#             code = random.randint(10000, 99999)
+#             send_otp(phone_number, code)
+#             request.session["register_form"] = {"phone_number": phone_number}
+#             return redirect("verify_code")
 
 
-class Verify(View):
-    template = "verify.html"
-    code_form = OtpCodeForm
+# class Verify(View):
+#     template = "verify.html"
+#     code_form = OtpCodeForm
 
-    def get(self, request):
-        form = self.code_form()
-        return render(request, self.template, {"form": form})
+#     def get(self, request):
+#         form = self.code_form()
+#         return render(request, self.template, {"form": form})
 
-    def post(self, request):
-        user_phone = request.session["register_form"]["phone_number"]
-        form = self.form(request.POST)
+#     def post(self, request):
+#         user_phone = request.session["register_form"]["phone_number"]
+#         form = self.form(request.POST)
 
-        if form.is_valid():
-            print("fine")
+#         if form.is_valid():
+#             print("fine")
