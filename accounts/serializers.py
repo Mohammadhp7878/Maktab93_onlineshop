a4f727cc99cb2e47cf80edb7a82c044482b89b90
mohammad <mohammadhp858@gmail.com>
2023-08-12 11:47:18 +0330

    feat: add serializer for phone otp verification

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,7 +3,16 @@ from . import models
         
         
 
+class PhoneSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.OtpCode
+        fields = ['phone_number']
+    
+    
 class OtpSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OtpCode
         fields = '__all__'
+
+    
