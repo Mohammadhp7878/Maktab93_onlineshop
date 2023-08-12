1b0789789591fbcf7a8e16a9e45a396f21b1e3a4
mohammad <mohammadhp858@gmail.com>
2023-08-12 10:06:17 +0330

    fix: change otp serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
 from . import models
-
-
-class PhoneNumberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.OtpCode
-        fields = ["phone_number"]
         
         
 
 class OtpSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OtpCode
-        fields = ["code"]
+        fields = '__all__'
