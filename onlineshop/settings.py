2b79f2ee90ef39e2d506710bb40be76e710798e1
mohammadhp <mohammadhp858@gmail.com>
2023-08-13 13:59:47 -0400

    fix: some minor changes

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -152,9 +152,13 @@ INTERNAL_IPS = [
 ]
 
 
+
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',  
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
