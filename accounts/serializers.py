cc5df640b185482ec9dc3e54091198876bf38dce
mohammadhp <mohammadhp858@gmail.com>
2023-08-13 13:54:54 -0400

    fix: fixing phone number serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from . import models
+import re
 
 
-class PhoneSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.OtpCode
-        fields = ["phone_number"]
+class PhoneSerializer(serializers.Serializer):
+    phone_number = serializers.CharField()
 
+    def validate_phone_number(self, phone):
+        print(phone)
+        phone_pattern = r'(0|\+98)?([ ]|-|[()]){0,2}9[0|1|2|3|4|9]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}'
 
-class OtpSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.OtpCode
-        fields = "__all__"
+        if not re.fullmatch(phone_pattern, phone):
+            raise serializers.ValidationError("Invalid phone number format")
+
+        return phone
