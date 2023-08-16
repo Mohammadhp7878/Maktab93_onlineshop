6b200b42f1ef4cc40ae49c26778248ab119944be
mohammadhp <mohammadhp858@gmail.com>
2023-08-16 07:24:17 -0400

    feat: add url for login and verify

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login_api/', views.LoginAPI.as_view(), name='login'),
-    path('verify_api/', views.VerifyAPI.as_view(), name='verify_code'),
+    path('login/', views.LoginPage.as_view(), name='login_page'),
+    path('verify/', views.VerifyPage.as_view(), name='verify_page'),
+    path('login_api/', views.LoginAPI.as_view(), name='login_api'),
+    path('verify_api/', views.VerifyAPI.as_view(), name='verify_api'),
 ]
