73c43cc26be7df30a36b35c2fe96a63f8d759eaf
mohammadhp <mohammadhp858@gmail.com>
2023-08-16 07:33:28 -0400

    feat: add profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('profile/', views.ProfilePage.as_view(), name='profile'),
     path('login/', views.LoginPage.as_view(), name='login_page'),
     path('verify/', views.VerifyPage.as_view(), name='verify_page'),
     path('login_api/', views.LoginAPI.as_view(), name='login_api'),
