c9f7339a1fa0091f7eb672f0b9d5d54ed332205f
mohammadhp <mohammadhp858@gmail.com>
2023-08-16 07:23:30 -0400

    feat: create token, restrict login url

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from django.core.cache import cache
+from django.contrib.auth import login
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from utilize import send_otp
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 from hashlib import sha256
 import random
+from utilize import send_otp
 from .models import User
 from .serializers import PhoneSerializer, CodeSerializer
 
+class LoginPage(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'login.html')
+
+
+class VerifyPage(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'verify.html')
 
 
 class LoginAPI(APIView):
     def post(self, request):
+        if request.user.is_authenticated:
+            return Response({"message": "You are already logged in."}, status=400)
         serializer = PhoneSerializer(data=request.data)
         if serializer.is_valid():
             phone_number = serializer.validated_data["phone_number"]
@@ -36,8 +54,13 @@ class VerifyAPI(APIView):
             phone = request.COOKIES.get("phone_number")
             saved_code = cache.get(phone)
             if saved_code and saved_code == user_hashed_code:
-                User.objects.get_or_create(phone_number=phone)
-                response = Response({"message": f"Welcome {phone}"})
+                user_instance, created = User.objects.get_or_create(phone_number=phone)
+                print(user_instance)
+                login(request, user_instance)
+                refresh_token = RefreshToken.for_user(user_instance)
+                access_token = str(refresh_token.access_token)
+                response = Response({"message": f"Welcome {phone}", "token": access_token})
+                print(access_token)
                 response.delete_cookie("phone_number")
                 return response
             else:
